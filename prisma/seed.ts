03e467fa23a7d0d095f2aab349b88d6ab9c38271
Michael Shingo Crawford <mcrawford5376@gmail.com>
2026-03-22 10:05:27 +0900

    setup prisma and docker

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function main() {
+  // create data here
+}
+
+main();
