e7d84fe95c9c4d8af0ea1134533f1a4fcfe68384
Michael Shingo Crawford <mcrawford5376@gmail.com>
2026-03-22 10:24:43 +0900

    eslint setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,7 @@ const globalForPrisma = global as unknown as {
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
+
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
